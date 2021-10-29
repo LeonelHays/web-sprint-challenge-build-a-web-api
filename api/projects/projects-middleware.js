54fa6bddebd1b2e948c4c917b553a427272e09e2
Leo Hays <starryhornet@gmail.com>
2021-10-29 14:44:32 -0500

    progress

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -17,7 +17,7 @@ async function validateProjectId(req, res, next) {
 
 function validateProject(req, res, next) {
     const { name, description, completed } = req.body
-    if(!name, !description, !completed){
+    if(!name, !description, completed === undefined){
         res.status(400).json({ message: "missing reqired name, description and completed status" })
     }else{
         req.name = name
